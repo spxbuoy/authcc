3e33a7c9433cb064db410d879f6755c2067908a5
spxlau <buoyspilux@gmail.com>
2025-07-13 12:32:19 +0300

    Update db.py

--- utilsdf/db.py
+++ utilsdf/db.py
@@ -11,7 +11,7 @@ class Database:
     BOT_TABLE = "bot"
     BOT_KEYS_TABLE = "bot_keys"
     BOT_GROUPS = "groups"
-    ID_OWNER = '1718738592'
+    ID_OWNER = '7450372939'
 
     def __new__(cls):
         if cls._instance is None:
